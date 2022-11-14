e975276b51415f73a4b6c19ca71a0b33b6f7dcce
Eduarda Isabelle <eduardaisabelle1818@gmail.com>
2022-11-14 07:43:11 -0300

    iniciando em variaveis

---
+++ variveis/Variaveis.java
@@ -0,0 +1,12 @@
+package variveis;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int nome123 = 23;
+        float _salarioBase = 1000.56f;
+        //String 1erro; // Erro de compilação por iniciar com caractere numérico.
+    
+        System.out.println(nome123); 
+        System.out.println(_salarioBase); 
+    }
+}

4e9ad444c6bdf4fb13a7ecd0fc8f53066ad80270
Eduarda Isabelle <eduardaisabelle1818@gmail.com>
2022-11-15 06:11:00 -0300

    atualizando arquivo

---
+++ variveis/Exercicio01.java
@@ -0,0 +1,17 @@
+package variveis;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        String nomePessoa = "Vitoria";
+        String enderecoPessoa = "Av. Tiradentes, N° 589 - B";
+        int idadePessoa = 23;
+        char estadoCivil = 'S';
+        float salarioPessoa = 9012.00f;
+        
+        System.out.println("Nome: " + nomePessoa);
+        System.out.println("Endereço: " + enderecoPessoa);
+        System.out.println("Idade: " + idadePessoa);
+        System.out.println("Estado civil: " + estadoCivil);
+        System.out.println("Salário: " + salarioPessoa);
+    }
+}

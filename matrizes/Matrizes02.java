614140bd01dfb1daa0ddbadfe8bdbaa58efc25a9
Eduarda Isabelle <eduardaisabelle1818@gmail.com>
2022-12-13 07:58:18 -0300

    matrizes

---
+++ matrizes/Matrizes02.java
@@ -0,0 +1,13 @@
+package matrizes;
+
+public class Matrizes02 {
+    public static void main(String[] args) {
+        double[][] notasAlunos1 = {{7,8,9,10}, {8,6,7,10}};
+        for (int i = 0; i < notasAlunos1.length; i++) {
+            for (int j = 0; j < notasAlunos1[i].length; j++) {
+                System.out.println(notasAlunos1[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
